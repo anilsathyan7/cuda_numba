4213ac942d24e3ada9f15dc760e15c7f79c2127d
anilsathyan <anilsathyan7@gmail.com>
2017-05-18 15:36:33 +0530

    Update numpy_add.py

--- numpy_add.py
+++ numpy_add.py
@@ -2,14 +2,11 @@ from __future__ import print_function
 
 import numpy as np
 
-
-from numba import cuda
 from numba import jit
-
 from timeit import default_timer as time
 
 
-
+#sum-up function
 def sum(a, b, c):
     for i in range(N):
         c[i] = a[i] + b[i]
@@ -18,11 +15,13 @@ griddim = 146432, 1
 blockdim = 1024, 1, 1
 N = griddim[0] * blockdim[0]
 print("N", N)
-#cuda_sum_configured = cuda_sum.configure(griddim, blockdim)
+
+#initialize arrays: 1D
 a = np.array(np.random.random(N), dtype=np.float64)
 b = np.array(np.random.random(N), dtype=np.float64)
 c = np.empty_like(a)
 
+#record processing time
 ts = time()
 sum(a, b, c)
 te = time()
