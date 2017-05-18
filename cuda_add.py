84c6ebfdf6c6af40f38ee7ea5411b685218d0603
anilsathyan <anilsathyan7@gmail.com>
2017-05-18 15:39:29 +0530

    Update cuda_add.py

--- cuda_add.py
+++ cuda_add.py
@@ -3,24 +3,29 @@ from __future__ import print_function
 import numpy as np
 
 from numba import cuda
-
 from timeit import default_timer as time
 
-
+#sum up function with cuda_jit decorator
 @cuda.jit("(float64[:], float64[:], float64[:])")
 def cuda_sum(a, b, c):
     i = cuda.grid(1)
     c[i] = a[i] + b[i]
 
+#set up grid and block dimensions    
 griddim = 146432, 1
 blockdim = 1024,1,1
 N = griddim[0]*griddim[1] * blockdim[0] * blockdim[1] * blockdim[2]
 print("N", N)
+
+#call cuda_sum kernel
 cuda_sum_configured = cuda_sum.configure(griddim, blockdim)
+
+#initialize numpy arrays
 a = np.array(np.random.random(N), dtype=np.float64)
 b = np.array(np.random.random(N), dtype=np.float64)
 c = np.empty_like(a)
 
+#record processing time
 ts = time()
 cuda_sum_configured(a, b, c)
 te = time()
